23c071db5234e408ae95c455b64a2e44ca2ab6cc
abolfazlnezami2000 <abolfazlnezami@gmail.com>
2021-02-27 19:08:28 +0330

    [Fix] = Fix background ui (SafeArea)

--- lib/core/widgets/background.dart
+++ lib/core/widgets/background.dart
@@ -13,46 +13,48 @@ class Background extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Container(
-      width: double.infinity,
-      height: size.height,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            child: SvgPicture.asset(
-                "assets/images/top1.svg",
-                width: size.width
+    return SafeArea(
+      child: Container(
+        width: double.infinity,
+        height: size.height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: SvgPicture.asset(
+                  "assets/images/top1.svg",
+                  width: size.width
+              ),
             ),
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            child: SvgPicture.asset(
-                "assets/images/top2.svg",
-                width: size.width
+            Positioned(
+              top: 0,
+              left: 0,
+              child: SvgPicture.asset(
+                  "assets/images/top2.svg",
+                  width: size.width
+              ),
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: SvgPicture.asset(
-                "assets/images/bottom1.svg",
-                width: size.width
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: SvgPicture.asset(
+                  "assets/images/bottom1.svg",
+                  width: size.width
+              ),
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: SvgPicture.asset(
-                "assets/images/bottom2.svg",
-                width: size.width
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: SvgPicture.asset(
+                  "assets/images/bottom2.svg",
+                  width: size.width
+              ),
             ),
-          ),
-          child
-        ],
+            child
+          ],
+        ),
       ),
     );
   }
